2838e5d21fe89430410053ccb8f186517573c24c
FR4NKESTI3N <aggarwal.yash2011@gmail.com>
2018-01-20 01:43:24 +0530

    Completed UVa/10264

--- 10000-99999/10264.cpp
+++ 10000-99999/10264.cpp
@@ -1,33 +1,27 @@
 #include <iostream>
-#include <bitset>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
 int main(void){
-    int n, temp;
-    while(cin>>n){
-        vector<int> a(n + 1, 0), b(n+1,0), c(n,0);
-        for(int i = 0; i < (1<<n); i+= 1){
-            cin >> temp;
-            bitset<32> k(i);
-            cout<<k<<' ';
-            a[k.count()] += temp;
+    int n, MAX;
+    while(cin >> n){
+        MAX = 0;
+        vector<int> weights((1 << n) + 1), potencies((1 << n) + 1);
+        for(int i = 0; i < (1 << n); ++i)
+            cin>>weights[i];
+        for(int i = 0; i < (1 << n); ++i){
+            for(int bit = 0; bit < n; ++bit){
+                potencies[i] += weights[i ^ (1 << bit)];
+            }
         }
-        cout<<'\n';
-        b[0] = a[1];
-        b[n] = a[n-1];
-        for(int i = 1; i < n; i++){
-            b[i] = a[i-1] + a[i+1];
+        for(int i = 0; i < (1 << n); ++i){
+            for(int bit = 0; bit < n; ++bit){
+                MAX = max(MAX, potencies[i] + potencies[(i ^ (1 << bit))]);
+            }
         }
-        for(auto i : b)
-            cout<<i<<' ';
-        cout<<'\n';
-        for(int i = 0; i < n; i++){
-            c[i] = b[i]+b[i+1];
-        }
-        cout<<*max_element(c.begin(), c.end())<<'\n';
+        cout<<MAX<<'\n';
     }
     return 0;
 }
